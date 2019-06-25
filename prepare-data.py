823c78e0f142e3c7544a75d9800c66c5771bd11d
Thanga Palanisamy <thanga.palanisamy@gmail.com>
2019-06-25 15:54:15 -0400

    add prepare-data

---
+++ prepare-data.py
@@ -0,0 +1,3 @@
+from brats.preprocess import convert_brats_data
+
+convert_brats_data("C:\\Workspace\\tf\\datasets\\BRATS2018\\data\\training", "C:\\Workspace\\tf\\datasets\\BRATS2018\\data\\training-processed")
